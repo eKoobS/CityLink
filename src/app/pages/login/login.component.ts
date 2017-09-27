90ce1be045de6cc8f79e5daa61ab9307688aadd5
juanb <juanbracamontes02@gmail.com>
2017-09-27 12:12:31 -0700

    Cambie el registrate

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  ShowVerifyPass:boolean=false;
+  ShowPassRegister:boolean=false;
 
   view:string = "login";
 
