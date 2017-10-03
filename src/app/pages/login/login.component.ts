7c7e8abd96ce12e9e464339bcd0c936e6b972988
juanb <juanbracamontes02@gmail.com>
2017-10-03 12:17:51 -0700

    algo nuevo

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -8,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   ShowVerifyPass:boolean=false;
   ShowPassRegister:boolean=false;
-
   view:string = "login";
   showPassLogin:boolean=false;
+
+  validar(){
+    console.log("Formulario Validado");
+  }
 
   constructor() { }
 
