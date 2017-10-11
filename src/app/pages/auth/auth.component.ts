cfee744fa4310bae0848fa7003274a4c336a97b9
juanbracamontes <juanbracamontes02@gmail.com>
2017-10-11 15:57:00 -0700

    vista auth verificarEmail

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,26 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AngularFireAuth} from 'angularfire2/auth';
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+    selector: 'app-auth',
+    templateUrl: './auth.component.html',
+    styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent implements OnInit {
 
-  parametro:string;
+    view: string;
 
-  constructor() { }
+    animatedIcon:boolean=true;
+    userInfoBasic:any;
 
-  ngOnInit() {
-  }
 
-    getParameterByName(name:string) {
+    constructor(private afAuth: AngularFireAuth) {
+        this.view=this.getParameterByName("mode");
+        if(this.view=="verifyEmail"){
+            this.verifyEmail();
+        }
+
+        setInterval(() => {
+            this.animatedIcon = !this.animatedIcon;
+        }, 2000);
+        afAuth.auth.onAuthStateChanged((user)=> {
+            // console.log(user)
+            this.userInfoBasic=user;
+        });
+    }
+    
+    ngOnInit() {
+    }
+
+    verifyEmail() {
+        this.afAuth.auth.applyActionCode(this.getParameterByName("oobCode")).then(response => {
+
+        }).catch((error: any) => {
+            console.log(error.code);
+        })
+
+    }
+
+    getParameterByName(name: string) {
         name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
         let regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
             results = regex.exec(location.hash);
-        this.parametro = results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+        return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 
-        debugger;
     }
 
 }
