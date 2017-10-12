6c85ffd5bc48df5e9bb6e888443c4d78aec97ec5
juanbracamontes <juanbracamontes02@gmail.com>
2017-10-12 14:12:07 -0700

    errores terminados

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {AngularFireAuth} from 'angularfire2/auth';
+import {alertService} from '../../services/alert.service';
 
 @Component({
     selector: 'app-auth',
@@ -14,7 +15,9 @@ export class AuthComponent implements OnInit {
     userInfoBasic:any;
 
 
-    constructor(private afAuth: AngularFireAuth) {
+    constructor(private afAuth: AngularFireAuth,
+                private alertService:alertService) {
+
         this.view=this.getParameterByName("mode");
         if(this.view=="verifyEmail"){
             this.verifyEmail();
@@ -32,12 +35,36 @@ export class AuthComponent implements OnInit {
     ngOnInit() {
     }
 
-    verifyEmail() {
-        this.afAuth.auth.applyActionCode(this.getParameterByName("oobCode")).then(response => {
 
-        }).catch((error: any) => {
-            console.log(error.code);
+    verifyEmail(){
+        // this.afAuth.auth.applyActionCode(code);
+        this.afAuth.auth.applyActionCode(this.getParameterByName("oobCode")).then(response =>{
+
+        }).catch((error:any) =>{
+            this.getFirebaseErrors(error.code);
         })
+    }
+
+    getFirebaseErrors(error:string){
+        switch( error ){
+            case 'auth/expired-action-code':
+                this.alertService.confirm("Oooops!, huston we have a problem!","Este enlace ya ha sido utilizado");
+                break;
+
+            case 'auth/invalid-action-code':
+                this.alertService.confirm("Enlace invalido","Este enlace no existe, intente con otro");
+                break;
+
+            case 'auth/user-disabled':
+                this.alertService.confirm("Usuario deshabilitado","Tu usuario ha sido bloqueado por " +
+                                                "alguna razon contacta al administrador");
+                break;
+
+            case 'auth/user-not-found':
+                this.alertService.confirm("Usuario no encontrado","No pudimos enviarte el codigo de verificacion" +
+                    " debido a que no encontramos tu usuario");
+                break;
+        }
 
     }
 
